8760ee561cd0d5c9eb4afc0e6e1ed10646b3ae55
delose <delossantos.eugene@gmail.com>
2024-09-25 23:54:32 +0800

    design patterns [wip]

---
+++ design-patterns/src/main/java/com/delose/java/lts/design_patterns/App.java
@@ -0,0 +1,12 @@
+package com.delose.java.lts.design_patterns;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
