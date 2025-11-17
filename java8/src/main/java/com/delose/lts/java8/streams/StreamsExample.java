6d423b7261d174f7ad3ca002d04f237bf7274b3c
delose <delossantos.eugene@gmail.com>
2025-11-17 17:47:55 +0800

    filter names sample

--- java8/src/main/java/com/delose/lts/java8/streams/StreamsExample.java
+++ java8/src/main/java/com/delose/lts/java8/streams/StreamsExample.java
@@ -9,4 +9,12 @@ public class StreamsExample {
                 .filter(name -> name.startsWith("J"))
                 .collect(Collectors.toList());
     }
+
+    public static void main(String[] args) {
+        List<String> stringList = List.of(
+                "John", "Jenkins", "Karl", "Lonnie", "Gustaf", "Candicer");
+        StreamsExample s = new StreamsExample();
+        List<String> namesThatStartsWithJ = s.filterNames(stringList);
+        System.out.println(namesThatStartsWithJ);
+    }
 }
