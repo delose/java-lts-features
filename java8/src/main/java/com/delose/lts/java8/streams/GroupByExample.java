37dc05ceb42854d180f4a64bf4af95474a73c418
delose <delossantos.eugene@gmail.com>
2025-11-21 22:52:47 +0800

    group by example

---
+++ java8/src/main/java/com/delose/lts/java8/streams/GroupByExample.java
@@ -0,0 +1,26 @@
+package com.delose.lts.java8.streams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+record Book(String language, String title) {}
+
+public class GroupByExample {
+
+    public static void main(String[] args) {
+        List<Book> books = List.of(
+                new Book("Spanish", "Don Quixote"),
+                new Book("Spanish", "La Sombra del Viento"),
+                new Book("English", "1984")
+        );
+
+        Map<String, Long> result = books.stream()
+                .collect(Collectors.groupingBy(
+                        Book::language,
+                        Collectors.counting()
+                ));
+
+        System.out.println(result);
+    }
+}
