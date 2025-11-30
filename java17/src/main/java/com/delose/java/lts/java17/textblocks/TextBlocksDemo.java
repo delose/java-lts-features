8b293056e2d4ec078dd3adac97bb585f4fcc9f19
delose <delossantos.eugene@gmail.com>
2025-11-30 10:34:33 +0800

    text blocks demo

---
+++ java17/src/main/java/com/delose/java/lts/java17/textblocks/TextBlocksDemo.java
@@ -0,0 +1,12 @@
+package com.delose.java.lts.java17.textblocks;
+
+public class TextBlocksDemo {
+    public static void main(String[] args) {
+        String multiLineText = """
+            This is the first line.
+            This is the second line.
+            And this is the third line.
+            """;
+        System.out.println(multiLineText);
+    }
+}
