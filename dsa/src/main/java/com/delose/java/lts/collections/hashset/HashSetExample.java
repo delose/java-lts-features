976127ec5b56384c1958204e2f895c0d27eff71f
delose <delossantos.eugene@gmail.com>
2025-12-05 07:52:42 +0800

    HashSetExample

---
+++ dsa/src/main/java/com/delose/java/lts/collections/hashset/HashSetExample.java
@@ -0,0 +1,19 @@
+package com.delose.java.lts.collections.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+record User(String id, String name) {}
+class Book {
+    String title, author;
+
+}
+
+public class HashSetExample {
+    static void main(String[] args) {
+        Set<User> s = new HashSet<>();
+        s.add(new User("1", "Ziggy"));
+        s.add(new User("1", "Ziggy"));
+        System.out.println(s);
+    }
+}
