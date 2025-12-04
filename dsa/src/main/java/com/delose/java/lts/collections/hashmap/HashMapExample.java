130a191d90a64262930f71b12a252d57d879ccc4
delose <delossantos.eugene@gmail.com>
2025-12-05 07:52:24 +0800

    HashMapExample

---
+++ dsa/src/main/java/com/delose/java/lts/collections/hashmap/HashMapExample.java
@@ -0,0 +1,17 @@
+package com.delose.java.lts.collections.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    static void main() {
+
+        Map<String, String> m = new HashMap<>();
+        m.put("k", "a");
+        m.put(null, "a");
+        System.out.println(m);
+        m.put("k", "b");
+        m.put(null, "b");
+        System.out.println(m);
+    }
+}
