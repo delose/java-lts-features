ca36749ecfe68e1907a63c512f9e9c5532459b97
delose <delossantos.eugene@gmail.com>
2025-12-05 08:42:06 +0800

    replace wildcard import

--- java8/src/main/java/com/delose/lts/java8/streams/StreamsExample.java
+++ java8/src/main/java/com/delose/lts/java8/streams/StreamsExample.java
@@ -1,6 +1,11 @@
 package com.delose.lts.java8.streams;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.Map;
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
