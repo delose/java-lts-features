042bd64dacc23cf1d28ecb3aa2c3a461d9ff24f7
delose <delossantos.eugene@gmail.com>
2025-12-13 07:54:34 +0800

    Vector sample

---
+++ dsa/src/main/java/com/delose/java/lts/collections/vector/VectorExample.java
@@ -0,0 +1,27 @@
+package com.delose.java.lts.collections.vector;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<String> fruits = new Vector<>();
+        fruits.add("Grapes");
+        fruits.add("Mango");
+        System.out.println(fruits);
+
+        String grapes = fruits.get(0);
+        System.out.println(grapes);
+
+        Iterator<String> iterator = fruits.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println("iterator.next(): " + iterator.next());
+        }
+
+        fruits.remove("Mango");
+        System.out.println(fruits);
+        System.out.println("size: " + fruits.size());
+        System.out.println("capacity: " + fruits.capacity());
+    }
+}
