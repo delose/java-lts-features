1f7e9dd9b465ff79d4ebeff80ac54fef1b50002c
delose <delossantos.eugene@gmail.com>
2026-02-16 06:37:55 +0800

    feat: add StringOperatorsExample class

---
+++ java8/src/main/java/com/delose/lts/java8/string/StringOperatorsExample.java
@@ -0,0 +1,17 @@
+package com.delose.lts.java8.string;
+
+public class StringOperatorsExample {
+    static void main() {
+
+        String str = "Hello World";
+        String EMPTY = " ";
+
+        String str1 = str.split(EMPTY)[0];
+        String str2 = str.split(EMPTY)[1];
+
+//        String randomStrToAppend = new Random().;
+//        String randomStr = str.replace(str, randomStrToAppend);
+//
+//        System.out.println("--Random String: ".concat(randomStr));
+    }
+}
